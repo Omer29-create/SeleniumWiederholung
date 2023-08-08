068106588eb42c353988b4b74d344c36cf4aec1e
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2023-08-08 23:05:57 +0200

    Add files via upload

--- src/test/java/day07_Junit/C03_BeforeAfter.java
+++ src/test/java/day07_Junit/C03_BeforeAfter.java
@@ -1,5 +1,35 @@
 package day07_Junit;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
 public class C03_BeforeAfter {
-    WebDriver
+    WebDriver driver;
+    @Before
+    public void setUp () {
+        driver = new ChromeDriver(new ChromeOptions().addArguments("--remote-allow-origins=*"));
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+
+    @After
+    public void tearDown () throws Exception {
+        driver.close();
+    }
+
+    @Test
+    public void test01() {
+        driver.get("https://techproeducation.com");
+    }
+
+    @Test
+    public void test02() {
+        driver.get("https://amazon.com");
+    }
 }
